fa8813f586535a282c714b2a6e150d7cd9afb589
sunnoy <sunnoy@users.noreply.github.com>
2025-08-06 15:34:08 +0800

    fix

--- update-derper.sh
+++ update-derper.sh
@@ -4,7 +4,7 @@
 DATE=$(date +%Y%m%d)
 
 # 设置工作目录（包含 docker-compose.yml 的目录）
-WORKDIR="/opt/derper"
+WORKDIR="/opt/tailscale-derper"
 
 # 替换 docker-compose.yml 中的镜像标签
 sed -i "s|\(ghcr.io/sunnoy/tailscale-derper:\)[0-9]\+|\1${DATE}|g" "$WORKDIR/docker-compose.yml"
